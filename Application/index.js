9d8bb5627cf343d250df42a812e8f3c2a5c1cc97
Sanjaypramod <sanjaypramod.p@gmail.com>
2024-06-07 19:51:47 +0530

    added

--- Application/index.js
+++ Application/index.js
@@ -3,5 +3,6 @@ const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
   res.send('POC 0.4 urolime...')
+  res. send ('sanjay')
 })
 app.listen(3000, () => console.log('Server is up and running'));
